62072021b79b2856ab38bda5ec62b2877b53cc31
ecarry <lianshiliang93@gmail.com>
2025-04-29 08:55:08 +0000

    chore: docs fold linux/docker

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,28 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
+import { DocsLayout, type DocsLayoutProps } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions, linkItems } from "@/app/layout.config";
+import { source } from "@/lib/source";
+import { FaLinux } from "react-icons/fa";
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [linkItems[linkItems.length - 1]],
+  sidebar: {
+    tabs: {
+      transform(option, node) {
+        const meta = source.getNodeMeta(node);
+        if (!meta) return option;
+
+        return {
+          ...option,
+          icon: <FaLinux size={28} />,
+        };
+      },
+    },
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }

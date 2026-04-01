2dff903482fb0846efd2ddd7af99ebce9431ba44
ecarry <lianshiliang93@gmail.com>
2026-04-01 15:26:31 +0000

    refactor: move sidebar icon logic from layout to source configuration

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,6 +1,9 @@
 import { docs } from "collections/server";
 import { loader } from "fumadocs-core/source";
 import { i18n } from "./i18n";
+import { FaLinux, FaDocker, FaNetworkWired } from "react-icons/fa";
+import { GrVmware } from "react-icons/gr";
+import { createElement } from "react";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
@@ -8,4 +11,18 @@ export const source = loader({
   // it assigns a URL to your pages
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (icon === "linux") {
+      return createElement(FaLinux);
+    }
+    if (icon === "docker") {
+      return createElement(FaDocker);
+    }
+    if (icon === "vmware") {
+      return createElement(GrVmware);
+    }
+    if (icon === "network") {
+      return createElement(FaNetworkWired);
+    }
+  },
 });
